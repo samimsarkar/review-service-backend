0f6c6face843b2cb8b6c19ff84bb85380c4cdfb4
samimsarkar <samimsarkar447@gmail.com>
2022-11-10 21:47:42 +0600

    Database Update part completed sucessfully

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ function verifyJWT(req, res, next) {
     if (!authHeader) {
         res.status(401).send({ message: 'unauthorized access' })
     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//         if (err) {
-//             res.status(401).send({ message: 'unauthorized access' })
-//         }
-//         req.decoded = decoded;
-//         next()
-//     })
-// }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 
 async function run() {
     try {
